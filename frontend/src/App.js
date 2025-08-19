59ca1b58f0f384b987c953c26f59c51078e86e69
Ram-Delos-Santos <ramjesler.delossantos@gmail.com>
2025-08-19 15:15:28 +0800

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import WelcomePage from './pages/main';
+import MainPage from './pages/welcome';
 
 function App() {
   return (
     <Routes>
-
+      <Route path='/' element={<WelcomePage/>}/>
+      <Route path='/home' element={<MainPage/>}/>
     </Routes>
   );
 }
