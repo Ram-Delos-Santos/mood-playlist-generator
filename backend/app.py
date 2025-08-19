4202b95811573c3016f09d53a15ace55d7f6349c
Ram-Delos-Santos <ramjesler.delossantos@gmail.com>
2025-08-19 15:14:57 +0800

    Setup temp API

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to Mood Playlist Generator"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
