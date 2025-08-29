187f4c51b877589668791ac8acc09377f7701b1c
Ram-Delos-Santos <ramjesler.delossantos@gmail.com>
2025-08-29 18:13:38 +0800

    Changed up main page

---
+++ frontend/src/pages/main/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./main.css";
+
+function MainPage() {
+    return (
+        <div class='main-container'>
+            <div>
+                Hello
+            </div>
+        </div>
+    );
+}
+export default MainPage;
+
